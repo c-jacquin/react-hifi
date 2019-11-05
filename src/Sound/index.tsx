2d7bc11d57a77266d7c430c1bca8d58b7d8c8004
Dave Thomas <dave@monstergfx.com>
2019-11-05 05:05:35 -0800

    fix(sound): change test in shouldUpdatePosition to permit position===0 (#4)
    fix(sound): add failing test for shouldUpdatePosition
    
    fix(sound): change test in shouldUpdatePosition to permit position===0
    
    fix(sound): clean up code
    
    fix #3

--- src/Sound/index.tsx
+++ src/Sound/index.tsx
@@ -154,7 +154,7 @@ export class Sound extends React.Component<SoundProps, SoundState> {
   private shouldUpdatePosition({ position }: SoundProps): boolean {
     const { position: nextPosition } = this.props;
 
-    if (position && nextPosition) {
+    if (position && (nextPosition || nextPosition === 0)) {
       const dif = nextPosition - position;
 
       return position > nextPosition || dif > 1;
